14dcf344276631ed6b940bcc3d4bf9272895d557
fanjili <fanjili@pset.suntec.net>
2017-04-25 14:53:10 +0800

    add test if

--- Test1/TestAlgorithm/CTestAlgorithm.cpp
+++ Test1/TestAlgorithm/CTestAlgorithm.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <array>
+#include <vector>
 
 CTestAlgorithm::CTestAlgorithm()
 {
@@ -18,9 +19,25 @@ CTestAlgorithm::Run()
 {
 	std::cout << "CTestAlgorithm::Run() start!" << std::endl;
 
+	// test non-modifying sequence operations
 	test_all_of();
 	test_any_of();
 	test_none_of();
+	test_for_each();
+	test_find();
+	test_find_if();
+	test_find_if_not();
+	test_find_end();
+	test_first_of();
+	test_adjacent_find();
+	test_count();
+	test_count_if();
+	test_mismatch();
+	test_equal();
+	test_is_permutation();
+	test_search();
+	test_search_n();
+
 
 	std::cout << "CTestAlgorithm::Run() end!" << std::endl;
 }
@@ -60,4 +77,160 @@ CTestAlgorithm::test_none_of()
 	else {
 		std::cout<<"CTestAlgorithm::test_none_of: there are negative element in the range"<<std::endl;
 	}
+}
+
+void opFunc(int i)
+{
+	std::cout<<' '<<i;
+}
+
+struct myClass
+{
+	void operator() (int i) {std::cout<<' '<<i;}
+} opojbect;
+
+void 
+CTestAlgorithm::test_for_each()
+{
+	std::vector<int> vTest;
+	vTest.push_back(10);
+	vTest.push_back(20);
+	vTest.push_back(30);
+
+	std::cout<< "CTestAlgorithm::test_for_each use function vTest containts: ";
+	for_each(vTest.begin(), vTest.end(), opFunc);
+	std::cout<< "\n";
+
+	std::cout<< "CTestAlgorithm::test_for_each use function object vTest containts: ";
+	for_each(vTest.begin(), vTest.end(), opojbect);
+	std::cout<< "\n";
+}
+
+void 
+CTestAlgorithm::test_find()
+{
+	int aTest[] = { 10, 20, 30, 40 };
+	int *p;
+	p = std::find(aTest, aTest+4, 30);
+	if (p != aTest+4) {
+		std::cout<< "CTestAlgorithm::test_find element fond in aTest:" << *p << '\n';
+	}
+	else {
+		std::cout<< "CTestAlgorithm::test_find element not fond in aTest\n";
+	}
+
+	std::vector<int> vTest(aTest, aTest+4);
+	std::vector<int>::iterator it;
+	it = std::find(vTest.begin(), vTest.end(), 30);
+	if (it != vTest.end()) {
+		std::cout<< "CTestAlgorithm::test_find element fond in vTest:" << *it << '\n';
+	}
+	else {
+		std::cout<< "CTestAlgorithm::test_find element not fond in vTest\n";
+	}
+}
+
+bool isOdd(int i)
+{
+	return ((i%2) == 1);
+}
+
+void 
+CTestAlgorithm::test_find_if()
+{
+	std::vector<int> vTest;
+	vTest.push_back(10);
+	vTest.push_back(20);
+	vTest.push_back(21);
+	vTest.push_back(30);
+	vTest.push_back(35);
+
+	std::vector<int>::iterator it;
+	it = std::find_if(vTest.begin(), vTest.end(), isOdd);
+	if (it != vTest.end()) {
+		std::cout<< "CTestAlgorithm::test_find_if element fond in vTest:" << *it << '\n';
+	}
+	else {
+		std::cout<< "CTestAlgorithm::test_find_if element not fond in vTest\n";
+	}
+}
+
+void 
+CTestAlgorithm::test_find_if_not()
+{
+	std::vector<int> vTest;
+	vTest.push_back(10);
+	vTest.push_back(20);
+	vTest.push_back(21);
+	vTest.push_back(30);
+	vTest.push_back(35);
+
+	std::vector<int>::iterator it;
+	it = std::find_if_not(vTest.begin(), vTest.end(), [](int i){return i%2;});
+	if (it != vTest.end()) {
+		std::cout<< "CTestAlgorithm::find_if_not element fond in vTest:" << *it << '\n';
+	}
+	else {
+		std::cout<< "CTestAlgorithm::find_if_not element not fond in vTest\n";
+	}
+}
+
+void 
+CTestAlgorithm::test_find_end()
+{
+
+}
+
+void 
+CTestAlgorithm::test_first_of()
+{
+
+}
+
+void 
+CTestAlgorithm::test_adjacent_find()
+{
+
+}
+
+void 
+CTestAlgorithm::test_count()
+{
+
+}
+
+void 
+CTestAlgorithm::test_count_if()
+{
+
+}
+
+void 
+CTestAlgorithm::test_mismatch()
+{
+
+}
+
+void 
+CTestAlgorithm::test_equal()
+{
+
+}
+
+void 
+CTestAlgorithm::test_is_permutation()
+{
+
+}
+
+void 
+CTestAlgorithm::test_search()
+{
+
+}
+
+void 
+CTestAlgorithm::test_search_n()
+{
+
 }
--- Test1/TestAlgorithm/CTestAlgorithm.h
+++ Test1/TestAlgorithm/CTestAlgorithm.h
@@ -13,6 +13,20 @@ private:
 	void test_all_of();
 	void test_any_of();
 	void test_none_of();
+	void test_for_each();
+	void test_find();
+	void test_find_if();
+	void test_find_if_not();
+	void test_find_end();
+	void test_first_of();
+	void test_adjacent_find();
+	void test_count();
+	void test_count_if();
+	void test_mismatch();
+	void test_equal();
+	void test_is_permutation();
+	void test_search();
+	void test_search_n();
 };
 
 #endif
